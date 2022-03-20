1b6fb9dfb499ee48223b81d965ebe9d069ed3156
Guest01 <chandu.dsc77@gmail.com>
2022-03-20 23:20:20 +0530

    modified the two-sum python file

---
+++ arrays/basic/two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h={}
+        for i in range(len(nums)):
+            if nums[i] in h:
+                return [h[nums[i]],i]
+            else:
+                h[target-nums[i]]=i
